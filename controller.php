46d489016f2813e784705a683929ecdaca9b44d3
Aaron Nous <aaronnous@Aarons-MacBook-Pro.local>
2018-09-03 21:34:35 +0300

    > query filtration in controller;

--- controller.php
+++ controller.php
@@ -1,3 +1,18 @@
 <?php
 
-    $shapes = mysqli_query($link, "SELECT * FROM $config[table];");
+    $where = '';
+    $filters = ['type' => 'string', 'color' => 'string', 'size' => 'int', 'stroke' => 'int'];
+
+    foreach ($filters as $filter => $type) {
+        if ($_GET[$filter]) {
+            $values = implode($type === 'int' ? ',' : "','", $_GET[$filter]);
+            if ($type === 'string') $values = "'$values'";
+            if ($values) {
+                if ($where !== '') $where .= " AND ";
+                $where .= "`$filter` IN ($values)";
+            }
+        }
+    }
+    if ($where) $where = "WHERE $where";
+
+    $shapes = mysqli_query($link, "SELECT * FROM $config[table] $where;");
---
+++ views/item.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: aaronnous
+ * Date: 2018-09-03
+ * Time: 9:16 PM
+ */
